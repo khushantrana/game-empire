6027cc9298785c70af9f224f2911d83274da1f02
khushantrana <73819768+khushantrana@users.noreply.github.com>
2023-07-13 16:37:05 +0530

    fetch the genres

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+import React, { ReactNode } from "react";
+interface Props {
+  children: ReactNode;
+}
+const GameCardContainer = ({ children }: Props) => {
+  return (
+    <Box borderRadius={10} overflow={"hidden"} width="250px">
+      {children}
+    </Box>
+  );
+};
+
+export default GameCardContainer;
